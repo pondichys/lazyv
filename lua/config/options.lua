ef2534fe9515e36b64554d4187daa78fa20dda13
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-06-18 19:43:28 +0200

    feat(options): add code folding based on treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Display entered command in the statusline of the window
-vim.opt.showcmdloc = "statusline"
+-- Configure code folding
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "expr"
 -- Highlight search results
 vim.opt.hlsearch = true
+-- Display entered command in the statusline of the window
+vim.opt.showcmdloc = "statusline"
 -- Add winbar with path to the file opened in the buffer
 vim.opt.winbar = "%=%m %f"
